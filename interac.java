5ae8346a8f7cc8e4898dc32708d847ebf46fcc57
devops1968 <gokeytravels@gmail.com>
2022-02-24 11:58:59 +0000

    my new commit

---
+++ interac.java
@@ -0,0 +1,8 @@
+echo "please enter your username"
+read username
+echo "please enter your password"
+read password
+echo "please confirm your password"
+read password
+echo "welcome to interac"
+date
